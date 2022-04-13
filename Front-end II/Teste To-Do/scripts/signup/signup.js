10f92222dc242a7f0b998a207228ed2c1c096817
GiMGalvao <giovannamgalvao@gmail.com>
2022-04-13 10:35:21 -0300

    CheckPoint II

--- Front-end II/Teste To-Do/scripts/signup/signup.js
+++ Front-end II/Teste To-Do/scripts/signup/signup.js
@@ -2,38 +2,35 @@ let nomeCriarConta = document.getElementById("nome");
 let apelidoCriarConta = document.getElementById("apelido");
 let emailCriarConta = document.getElementById("email");
 let senhaCriarConta = document.getElementById("senha");
-// let repetirSenhaCriarConta = getElementById("repetirSenha");
 let botaoCadastro = document.getElementById("botaoCadastro")
 
-
+let cadastro = false;
 
 botaoCadastro.addEventListener('click', function(event) {
 
     event.preventDefault();
 
     let dados = {
-        "firstName": "string",
-        "lastName": "string",
-        "email": "string",
-        "password": "string"
+        "firstName": nomeCriarConta.value,
+        "lastName": apelidoCriarConta.value,
+        "email": emailCriarConta.value,
+        "password": senhaCriarConta.value
       };
 
     
-    fetch('https://ctd-todo-api.herokuapp.com/v1/users',    {
+    fetch(`https://ctd-todo-api.herokuapp.com/v1/users`,    {
         method: 'POST',
-        body: dados,
         headers: {
-            'content-type':'application/json'
-
-        }
+            "Content-type" : 'application/json'
+        },
+        body: JSON.stringify(dados)
     
     })
 
-    .then(function(response){
-        return response.json()
+    .then(response => response.json())
 
-    })
-    
+       
  console.log(dados); 
 
 })
+

fe562c6eb1360128a103ae233ae6238daac04077
GiMGalvao <giovannamgalvao@gmail.com>
2022-04-25 17:35:28 -0300

    Primeiros exs

---
+++ Estudos/Udemy/152/scripts/152.js
@@ -0,0 +1,10 @@
+var texto = "Curso de JavaScript"
+
+var numeroInteiro = -5
+
+var numeroFracionado = 10.75
+
+var boolean = true
+
+
+document.write(numeroFracionado); //escreve diretamente no browse// 

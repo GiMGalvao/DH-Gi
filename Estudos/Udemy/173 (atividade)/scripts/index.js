77ce0201e590185123bfaa75e474a47cabc69ad1
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-04 16:57:48 -0300

    Estudo atualizado sobre funcao e evento

--- Estudos/Udemy/173 (atividade)/scripts/index.js
+++ Estudos/Udemy/173 (atividade)/scripts/index.js
@@ -1,60 +1,45 @@
-var nome = prompt("Digite aqui seu nome")
-var altura = prompt("Digite aqui sua altura em cm")
-var peso = prompt("Digite aqui seu peso")
+//recebe os parâmetros
+var nome = prompt('Informe o nome da pessoa');
+var altura = prompt('Informe a altura da pessoa em centímetros');
+var peso = prompt('Informe o pessoa da pessoa');
 
-altura = parseFloat(altura)
-peso = parseFloat(peso)
+//converte as cadeias de caracteres para dados do tipo float
+altura = parseFloat(altura);
+peso = parseFloat(peso);
 
-altura /=100 
+//abaixo é feito a conversão da altura de centímetros para metros
+altura = altura / 100;
 
-var resultadoImc = peso / (altura * altura)
+//realiza o cálculo de img
+var imc = peso / (altura * altura);
 
-if  (resultadoImc > 20 ){
-    document.write("ok")
+//testes condicionais para identificar a classificação do peso com base no imc
+var classificacao = '';
 
-}else {
+if(imc < 16){
+ classificacao = 'Baixo peso muito grave';
 
-    document.write("no")
+} else if(imc >= 16 && imc <= 16.99){
+ classificacao = 'Baixo peso grave';
+
+} else if(imc >= 17 && imc <= 18.49){
+ classificacao = 'Baixo peso';
+
+} else if(imc >= 18.50 && imc <= 24.99){
+ classificacao = 'Peso normal';
+
+} else if(imc >= 25 && imc <= 29.99){
+ classificacao = 'Sobrepeso';
+
+} else if(imc >= 30 && imc <= 34.99){ 
+    classificacao = 'Obesidade grau I';
+
+} else if(imc >= 35 && imc <= 39.99){
+ classificacao = 'Obesidade grau II';
+
+} else { 
+ classificacao = 'Obesidade grau III';
 }
 
-// if (resultadoImc < 16){
-
-//     document.write("Baixo peso muito grave")
-
-    
-// }else if (resultadoImc >= 16 && resultadoImc < 16,99){
-
-//     document.write("Baixo peso grave")
-
-    
-// }else if (resultadoImc >= 17 && resultadoImc < 18,49){
-
-//     document.write("Baixo peso")
-
-    
-// }else if (resultadoImc >= 18,50 && resultadoImc < 24,99){
-
-//     document.write("Peso Normal!!")
-
-    
-// }else if (resultadoImc >= 25 && resultadoImc < 29,99){
-
-//     document.write("Sobrepeso")
-
-    
-// }else if (resultadoImc >= 30 && resultadoImc < 34,99){
-
-//     document.write("Obesidade grau I")
-    
-// }else if (resultadoImc >= 35 && resultadoImc < 39,99){
-
-//     document.write("Obesidade grau II")
-    
-// }else{ 
-
-//     document.write("Obesidade grau III")
-
-// }
-
-
-console.log(resultadoImc);
+document.write(nome + ' possui índice de massa corporal igual a ' + imc + 
+'sendo classificado como:' + classificacao); 

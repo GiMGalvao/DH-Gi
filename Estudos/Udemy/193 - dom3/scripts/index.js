e12700c3fb0e6405b83f874a05d32e9b403d6310
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-08 20:46:28 -0300

    Dom + estilos

--- Estudos/Udemy/193 - dom3/scripts/index.js
+++ Estudos/Udemy/193 - dom3/scripts/index.js
@@ -2,3 +2,8 @@ var botaoVerde = document.getElementById('btn_green');
 var botaoAzule = document.getElementById('btn_blue');
 var botaoRosa = document.getElementById('btn_pink');
 
+function modificandoEstilo (corFundo){
+
+    document.getElementById('quadrado').style.backgroundColor = corFundo
+    
+}

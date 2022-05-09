95f7e0ed6359834c1ec2c9e5fcdf3556372c258c
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-09 16:13:11 -0300

    Atividade e calculadora

---
+++ Estudos/Udemy/196 - app1/scripts/script.js
@@ -0,0 +1,14 @@
+function calcular (tipo, valor){
+
+if(tipo === 'acao'){
+
+
+}else if (tipo === ' valor'){
+
+    var valorCampo = 
+    
+    document.getElementById('resultado').value = valor
+}
+
+
+}

aa882f02a2d15d45f729246bbd909405109af8dd
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-10 17:52:53 -0300

    calculadora

--- Estudos/Udemy/196 - app1/scripts/script.js
+++ Estudos/Udemy/196 - app1/scripts/script.js
@@ -2,12 +2,24 @@ function calcular (tipo, valor){
 
 if(tipo === 'acao'){
 
+    if(valor === 'c'){
+        document.getElementById('resultado').value = "";
 
-}else if (tipo === ' valor'){
+    }else if(valor === '+' || valor == '-' || valor === '*' || valor === '/' || valor === '.'){
+       
+        document.getElementById('resultado').value +=  valor
 
-    var valorCampo = 
-    
-    document.getElementById('resultado').value = valor
+    }else if(valor === '='){
+
+        var valorCampo = eval(document.getElementById('resultado').value) //eval converte string em uma operação que o js pode realizar
+        document.getElementById('resultado').value = valorCampo
+
+    }
+
+
+}else if (tipo === 'valor'){
+
+     document.getElementById('resultado').value +=  valor //ele pega o valor do resultado e concatena com ele mesmo
 }
 
 

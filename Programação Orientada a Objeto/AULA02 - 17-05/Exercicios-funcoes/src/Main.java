f17f7a7178b3684e1a4e06b833de9713326b0b3d
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-18 15:29:28 -0300

    novo bimestre

---
+++ Programação Orientada a Objeto/AULA02 - 17-05/Exercicios-funcoes/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    static int ehPrimo(int num) {
+
+        num = 10;
+
+        System.out.println("num = " + num*2);
+
+        return num *2;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}

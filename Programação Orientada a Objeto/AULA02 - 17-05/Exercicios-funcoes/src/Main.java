2f00df256ec099ebdc6be5158009705662feb31f
GiMGalvao <giovannamgalvao@gmail.com>
2022-05-29 19:12:29 -0300

    ok

--- Programação Orientada a Objeto/AULA02 - 17-05/Exercicios-funcoes/src/Main.java
+++ Programação Orientada a Objeto/AULA02 - 17-05/Exercicios-funcoes/src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
 
-    static int ehPrimo(int num) {
+    static boolean numeroDivisivel(int num1, int num2){
 
-        num = 10;
-
-        System.out.println("num = " + num*2);
-
-        return num *2;
-
+        System.out.println(num1%num2==0);
+        return num1%num2==0;
     }
 
 
 
     public static void main(String[] args) {
+
+        numeroDivisivel(int num1: 10, int num2: 20);
 
     }
 }

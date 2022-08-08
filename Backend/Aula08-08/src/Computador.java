5a6b595352dd617061751ef39762d06f2ec77834
GiMGalvao <giovannamgalvao@gmail.com>
2022-08-08 20:56:08 -0300

    aula 08/08

---
+++ Backend/Aula08-08/src/Computador.java
@@ -0,0 +1,9 @@
+public class Computador {
+
+    private String id;
+    private int ram;
+    private int hd;
+    private int contador;
+
+    //map: tem um ID e um valor. Ele identifica pelo ID qual posição esta aquele valor;
+}
